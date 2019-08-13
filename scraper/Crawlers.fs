e8674da0eeb43de381018c4b11ce9e79320f0cd5
JP3BGY <6635381+JP3BGY@users.noreply.github.com>
2019-08-13 16:37:12 +0900

    change Sleep timing

--- scraper/Crawlers.fs
+++ scraper/Crawlers.fs
@@ -13,9 +13,9 @@ let delCacheArray=[|
 |]
 
 let rec crawlerLoop ()=
-    Threading.Thread.Sleep(TimeSpan.FromHours(4.0))
     crawlerArray|>Array.map(fun x-> x())|>Async.Parallel|>Async.RunSynchronously|>ignore
     delCacheArray|>Array.map(fun x->x())|>ignore
+    Threading.Thread.Sleep(TimeSpan.FromHours(4.0))
     if Console.KeyAvailable then 
         match Console.ReadKey().Key with
         | ConsoleKey.Q -> ()
